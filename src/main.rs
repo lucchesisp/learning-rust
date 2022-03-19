844b055bfe1b9134b8899f2e900d5791e11c80d5
Gabriel Lucchesi <lucchesisp@gmail.com>
2022-03-19 04:33:35 -0300

    all files

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+fn main() {
+
+    // Variables
+    let number: i8 = 127;
+    println!("Number is {} and this length in bytes is {}", number, std::mem::size_of_val(&number));
+
+    let decimal: f64 = 32.1000005;
+    println!("Float number is {} and this length in bytes is {}", decimal, std::mem::size_of_val(&decimal));
+
+    let boolean: bool = false;
+    println!("Bool is {} and this length in bytes is {}", boolean, std::mem::size_of_val(&boolean));
+
+    let char: char = 'É';
+    println!("Char is {} and this length in bytes is {}", char, std::mem::size_of_val(&char));
+
+    // Constants
+
+    const PI: f32 = 3.14;
+    println!("Constant PI is {}", PI);
+
+    static mut _VARIABLE: i64 = 300;
+    unsafe {
+        println!("Static variable is {}", _VARIABLE);
+    }
+
+    // Functions
+
+    fn sum(n1: i32, n2: i32) -> i32 {
+        return n1 + n2;
+    }
+
+    println!("A soma de 1 + 5 = {}", sum(1, 5));
+
+    // Match Statments
+
+    let language: &str = "Rust";
+    let purpose: &str = match language {
+        "Rust" => "Low Level",
+        _ => "Undefined"
+    };
+
+    println!("The language is {} and this purpose is {}", language, purpose);
+
+    // Ownership
+
+    let string: &str = "Heap Allocation";
+    steal(string);
+
+    // println!(string) <==== This scope has lost owership
+
+    fn steal(string: &str) -> &str {
+        return string;
+    }
+
+}
